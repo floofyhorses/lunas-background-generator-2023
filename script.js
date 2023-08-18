6078dc2a2a81f6f5cfceda883cbc05745a74ba8e
floofyhorses <dearmirai.rm@gmail.com>
2023-08-18 06:46:07 -0700

    Remove getInitialGradient function due to duplicating task. Remove onload function.

--- script.js
+++ script.js
@@ -3,25 +3,9 @@ let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
 
-function getInitialGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
 
-	css.textContent = body.style.background + ";";
-}
-
-  document.onload(getInitialGradient()); //This makes picker same color as
-                                        //Initial bg and makes h3 say the 
-										//initial color.
-
-  function setGradient() {              //I want to make it so that the
-	                                    //color picker once more changes
-										//the color of the gradient, and
-										//makes the h3 say that value instead.
+function setGradient() {              
+	                    
 	body.style,background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
@@ -32,19 +16,9 @@ function getInitialGradient() {
 	css.textContent = body.style.background + ";";
 }
 
-// function setTextOnLoad() {     //2. Display the initial CSS linear gradient 
-// 	//property on page load. Trying, not working yet//
-// 	body.style.background = 
-// 	"linear-gradient(to right, " 
-// 	+ body;
 
-// 
-
-body.addEventListener("load", setGradient);
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
-// windows.onload("onload", setTextOnLoad); //2. Display the initial CSS linear gradient 
-// //property on page load. Trying, not working yet//
