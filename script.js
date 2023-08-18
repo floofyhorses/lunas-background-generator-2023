e05deef170ece68ff2ab87a4e0527a12d4b59c12
floofyhorses <dearmirai.rm@gmail.com>
2023-08-18 07:56:31 -0700

    Create new function randomNumber to generate random RGB combos on click. Incomplete as of yet/

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
-
+let button = document.querySelector(".button");
 
 function setGradient() {              
 	                    
@@ -16,10 +16,20 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function randomNumber(min,max) {
+	min = Math.ceil(min);
+    max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min) + min);
+	
+}
+
+
+
 window.addEventListener("document.onload", setGradient())
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
+button.addEventListener("click", randomNumber);
 
