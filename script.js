e65146939d963d946c4106a990fb605297a98f23
floofyhorses <dearmirai.rm@gmail.com>
2023-08-22 09:22:23 -0700

    Add function randomNumber to generate a random number between 0 and 255.

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector(".color1");
 let color2 = document.querySelector(".color2");
 let body = document.getElementById("gradient");
-let button = document.querySelector(".button");
+let button = document.querySelector("button");
 
 function setGradient() {              
 	                    
@@ -15,21 +15,65 @@ function setGradient() {
 
 	css.textContent = body.style.background + ";";
 }
+//Generating a random set of 3 numbers for each color value 
 
-function randomNumber(min,max) {
-	min = Math.ceil(min);
-    max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min) + min);
+// function randomNumber(max) {
 	
-}
+// 		return Math.floor(Math.random() * max);
+// 	  }
+	//   console.log(randomNumber(256));//This works but isnt what i need)
+	
+	  function randomNumber(min, max) {
+		return Math.floor(Math.random() * (max - min) + min);
+	  }
+	  randomNumber(0,256);
+	// Math.floor(Math.random() * (max - min) + min);
+	
+	
+	// while (randomNumber < 4) {
+	// 	for (i=0; i < 4; i++ ) {
+	// 	color1.value = randomNumber();
+	// 	color2.value = randomNumber(); 
+	// 	setGradient();
+	// 	console.log("Random number is working");
+		
+	// }
+	// return(randomNumber(0, 256));
+	// }
+
+
+//Setting new background after button click
+
+// function randomColor(randomNumber) {
+// 	color1.value = randomNumber();
+// 	color2.value = randomNumber(); 
+// 	setGradient();
+// 	setGradient(randomNumber) {
+
+// 	}
+ 
+// }
+// randomColor();
 
 
 
-window.addEventListener("document.onload", setGradient())
+	
+		
+	
+	     
+		   
+// Add event listeners to listen for actions and do the thing
 
 color1.addEventListener("input", setGradient);
 
 color2.addEventListener("input", setGradient);
 
+window.addEventListener("document.onload", setGradient());
+
 button.addEventListener("click", randomNumber);
+
+
+
+
+
 
