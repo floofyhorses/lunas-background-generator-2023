89cd377cd5f8ec57ce669c0524c84c1261a5f229
floofyhorses <dearmirai.rm@gmail.com>
2023-08-24 08:59:05 -0700

    Final version before cleaning up code. Apologies for few atomic commits along the way.

--- script.js
+++ script.js
@@ -12,24 +12,47 @@ function setGradient() {
 	+ ", " 
 	+ color2.value 
 	+ ")";
-
 	css.textContent = body.style.background + ";";
+	
 }
+
 //Generating a random set of 3 numbers for each color value 
 
-// function randomNumber(max) {
+
+	  
+
+	 function randomValue() {
+		    let r = Math.floor(Math.random() * 256);
+		    let g = Math.floor(Math.random() * 256);
+			let b = Math.floor(Math.random() * 256);
+			return "rgb(" + "#" + r+r+g+g+b+b + ")";
+		                  
 	
-// 		return Math.floor(Math.random() * max);
-// 	  }
-	//   console.log(randomNumber(256));//This works but isnt what i need)
-	
-	  function randomNumber(min, max) {
-		return Math.floor(Math.random() * (max - min) + min);
-	  }
-	  randomNumber(0,256);
-	// Math.floor(Math.random() * (max - min) + min);
-	
-	
+	     }
+		 randomValue();
+	///The above works!
+		
+	  
+	//   randomValue();
+	//   setGradient(randomValue);  //HERE IS IS WHERE THE CODE BREAKS
+	  //TypeError: Cannot read properties of null (reading 'styl
+	 
+	 function randomGradient() {
+		let randomColor1 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+		let randomColor2 = "#" + Math.floor(Math.random() * 16777215).toString(16);
+		color1.value = randomColor1;
+		color2.value = randomColor2;
+		
+		body.style.background = 
+	"linear-gradient(to right, " 
+	+ randomColor1
+	+ ", " 
+	+ randomColor2 
+	+ ")";
+	css.textContent = body.style.background + ";";
+
+	 }
+
 	// while (randomNumber < 4) {
 	// 	for (i=0; i < 4; i++ ) {
 	// 	color1.value = randomNumber();
@@ -70,7 +93,7 @@ color2.addEventListener("input", setGradient);
 
 window.addEventListener("document.onload", setGradient());
 
-button.addEventListener("click", randomNumber);
+button.addEventListener("click", randomGradient);
 
 
 
